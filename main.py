8ed747cc37707a7fe6018f9f62108c6db1c0c261
Trung Nguyen <nguyenviettrung1995@gmail.com>
2025-05-19 15:25:14 +0700

    Clean up except

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ def get_working_directory() -> str:
     """Get a temporary working directory"""
     return DIR
 
-@mcp.tool()
+# @mcp.tool()
 def clean_working_directory() -> str:
-    """Clean the temporary working directory"""
+    """Clean the temporary working directory completely"""
     
     for item in os.listdir(DIR):
         item_path = os.path.join(DIR, item)
@@ -26,6 +26,25 @@ def clean_working_directory() -> str:
             import shutil
             shutil.rmtree(item_path)
     return f"Cleaned all files and folders in {DIR}"
+
+@mcp.tool()
+def clean_working_directory_except(exclude: list[str]) -> str:
+    """Clean the temporary working directory except specified files/folders
+    
+    Args:
+        exclude: List of file/folder names to preserve
+    """
+    
+    for item in os.listdir(DIR):
+        if item in exclude:
+            continue
+        item_path = os.path.join(DIR, item)
+        if os.path.isfile(item_path):
+            os.remove(item_path)
+        elif os.path.isdir(item_path):
+            import shutil
+            shutil.rmtree(item_path)
+    return f"Cleaned all files and folders in {DIR} except {exclude}"
 
 # Start the server
 if __name__ == "__main__":
